72060364f05e740f85045125c1d1392b4d403f0a
Priyanka Lashkari <priyankalashkari236@gmail.com>
2024-10-06 18:42:30 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,7 +1,7 @@
 public class LeapYear {
     public static void main(String[] args) {
         int year = 2023;
-        if(year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
             System.out.println("Leap Year");
         } else {
             System.out.println("Not a Leap Year");
