de434c62db55bb648ccb039a711ee03600782b41
priyankalashkari <priyankalashkari236@gmail.com>
2025-01-29 15:59:00 +0530

    Basic Recursion

---
+++ BasicRecursion/SumOfFirstN.java
@@ -0,0 +1,19 @@
+package BasicRecursion;
+
+public class SumOfFirstN {
+    
+// Q. Write a program to print sum of First N natuaral number.
+
+    public static int sum(int n){
+        if(n == 0){
+            return 0;
+        }
+        int sum = n + sum(n - 1);
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = sum(n);
+        System.out.println(sum);
+    }
+}
